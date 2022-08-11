e5ae6a71411cbfeee7461a509353fcc4e8a857ac
raviharyo <raffiiphone99@gmail.com>
2022-08-11 10:42:59 +0700

    initialize

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import './assets/tailwind/index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/about' element={<AboutPage />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
