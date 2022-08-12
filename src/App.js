1cd571a47c75b0ecfa604303460e918f0bf9999e
raviharyo <raffiiphone99@gmail.com>
2022-08-12 21:29:35 +0700

    add feature check price

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import PricePage from './pages/PricePage';
 import './assets/tailwind/index.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/about' element={<AboutPage />}></Route>
+        <Route path='/shipment-price' element={<PricePage />}></Route>
       </Routes>
     </div>
   );
