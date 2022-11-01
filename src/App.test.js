b63f35abc523487284163e23c13ffe6b45913d9a
Laksamana Zaidan <laksamana.alfayyadh@amartha.com>
2022-11-01 22:09:55 +0700

    add testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+const RenderApp = () => {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+
+test("renders learn react link", () => {
+  render(<RenderApp />);
+  const linkElement = screen.getByText(/Deliver Your Goods/i);
   expect(linkElement).toBeInTheDocument();
 });
